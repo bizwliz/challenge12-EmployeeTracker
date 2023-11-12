a0f6ded8bb3b6419a605470d3955cc85b153912a
bizwliz <glissette1999@gmail.com>
2023-11-11 22:19:09 -0500

    updated schema, seeds, queries

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS  employeetrack_db;
-CREATE DATABASE  employeetrack_db;
+DROP DATABASE IF EXISTS employeetrack_db;
+CREATE DATABASE employeetrack_db;
 
-USE  employeetrack_db;
+USE employeetrack_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -12,24 +12,17 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
-    last name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    last_name VARCHAR(30) NOT NULL,  
+    role_id INT,
     manager_id INT,
 
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL
-
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,6 +4,28 @@ INSERT INTO department (name)
 VALUES ("Sales"),
        ("Engeineering"),
        ("Finance"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+       ("Legal");
+
+INSERT INTO role (title,salary,department_id)
+VALUES ("Sales Lead",10000,1),
+       ("Salesperson",80000,1),
+       ("Lead Engineer",150000,2),
+       ("Software Engineer",120000,2),
+       ("Account Manager",160000,3),
+       ("Accountant",125000,3),
+       ("Legal Team Lead",250000,4),
+       ("Lawyer",190000,4);
+
+INSERT INTO employee (first_name,last_name,role_id)
+VALUES ("John","Doe",1),
+       ("Mike","Chan",2),
+       ("Ashley","Rodriguez",3),
+       ("Kevin","Tupik",4),
+       ("Kunal","Singh",5),
+       ("Malia","Brown",6),
+       ("Sarah","Lourd",7),
+       ("Tom","Allen",8);
+
+UPDATE employee SET manager_id = 1 WHERE id=2;
+
 
