39f5cf1c2622fc2f6ca09048470e3d4c5040b3c4
bizwliz <glissette1999@gmail.com>
2023-11-11 23:49:22 -0500

    fixed bug, updated functions for add/view employee

--- index.js
+++ index.js
@@ -1,72 +1,128 @@
-const inquirer=require("inquirer");
-const mysql=require("mysq12");
-const {printTable}=require("console-table-printer");
-const { listenerCount } = require("process");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const { printTable } = require("console-table-printer");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port:3306
-})
+    port: 3306
+});
 
-db.connect(()=>{
-    mainMenu()
+db.connect(() => {
+    mainMenu();
+});
 
-})
-
-function mainMenu(){
+function mainMenu() {
     inquirer.prompt({
         type: "list",
         message: "What would you like to do?",
-        name: "Selection",
-        choices: ["View All Departments","View All Roles","View All Employees","Add A Department","Add A Role","Add An Employee","Update An Employee Role"]
+        name: "selection",
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update An Employee Role"]
     })
-    .then(answer=>{
-        if(answer.selection==="View All Departments"){
-            viewDepartments()
-        }else if(answer.selection==="View All Roles"){
-            viewRoles()
-        }else if(answer.selection==="View All Employees"){
-            viewEmployees()
-        }else if(answer.selection==="Add A Department"){
-            addDepartment()
-        }else if(answer.selection==="Add A Role"){
-            addRole()
-        }else if(answer.selection==="Add An Employee"){
-            addEmployee()
-        }else if(answer.selection==="Update An Employee Role"){
-            updateEmployeeRole()
+    .then(answer => {
+        if (answer.selection === "View All Departments") {
+            viewDepartments();
+        } else if (answer.selection === "View All Roles") {
+            viewRoles();
+        } else if (answer.selection === "View All Employees") {
+            viewEmployees();
+        } else if (answer.selection === "Add A Department") {
+            addDepartment();
+        } else if (answer.selection === "Add A Role") {
+            addRole();
+        } else if (answer.selection === "Add An Employee") {
+            addEmployee();
+        } else if (answer.selection === "Update An Employee Role") {
+            updateEmployeeRole();
         }
-    })
+    });
 }
 
-function viewDepartments(){
-
+function viewDepartments() {
+    console.log("Viewing departments. Not implemented yet.");
+    mainMenu();
 }
 
-function viewRoles(){
-
+function viewRoles() {
+    console.log("Viewing roles. Not implemented yet.");
+    mainMenu();
 }
 
-function viewEmployees(){
+function viewEmployees() {
+    const query = `
+        SELECT employee.id, employee.first_name, employee.last_name, title, name as department, salary, 
+        CONCAT(bosses.first_name, ' ', bosses.last_name) as manager 
+        FROM employee
+        LEFT JOIN role ON employee.role_id = role.id
+        LEFT JOIN department ON department.id = role.department_id
+        LEFT JOIN employee AS bosses ON employee.manager_id = bosses.id;`;
 
+    db.query(query, (err, data) => {
+        if (err) {
+            console.error("Error fetching employees:", err);
+        } else {
+            printTable(data);
+        }
+        mainMenu();
+    });
 }
 
-function addDepartment(){
 
+function addDepartment() {
+    console.log("Adding a department. Not implemented yet.");
+    mainMenu();
 }
 
-function addRole(){
-
+function addRole() {
+    console.log("Adding a role. Not implemented yet.");
+    mainMenu();
 }
 
-function addEmployee(){
-
+function addEmployee() {
+    db.query("SELECT id as value, title as name from role ", (err, roleData) => {
+        db.query("SELECT id as value, CONCAT(first_name, ' ', last_name) as name FROM employee WHERE manager_id is null", (err, ManagerData) => {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is the first name?",
+                    name: "first_name",
+                },
+                {
+                    type: "input",
+                    message: "What is the last name?",
+                    name: "last_name",
+                },
+                {
+                    type: "list",
+                    message: "Choose the following title:",
+                    name: "role_id",
+                    choices: roleData
+                },
+                {
+                    type: "list",
+                    message: "Choose the following title:",
+                    name: "manager_id",
+                    choices: ManagerData
+                },
+            ]).then(answer => {
+                db.query("INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)", [answer.first_name, answer.last_name, answer.role_id, answer.manager_id], err => {
+                    if (err) {
+                        console.error("Error adding employee:", err);
+                    } else {
+                        console.log("Employee added successfully.");
+                        viewEmployees();  // Call viewEmployees after adding an employee
+                    }
+                    mainMenu();
+                });
+            });
+        });
+    });
 }
 
-function updateEmployeeRole(){
-    
+function updateEmployeeRole() {
+    console.log("Updating employee role. Not implemented yet.");
+    mainMenu();
 }
